5fe97469069721730b7f647d6505f016b8d3aa0c
LeonardoCto <coutinholeonardo94@gmail.com>
2023-08-21 22:18:27 -0300

    CRUD COMPLETO EXCEPTION

---
+++ src/main/java/entidade/Produto.java
@@ -0,0 +1,83 @@
+package entidade;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//@Data
+@Entity
+@Table(name = "produtos") 
+public class Produto {
+	
+	@Id
+	private Integer id;
+	private String nome;
+	private String fabricante;
+	private Double valor;
+	private Double peso;
+	private LocalDate dataCadastro;
+	
+	public Produto(Integer id, String nome, String fabricante, Double valor, Double peso, LocalDate dataCadastro) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.fabricante = fabricante;
+		this.valor = valor;
+		this.peso = peso;
+		this.dataCadastro = dataCadastro;
+	}
+	
+	public Produto() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+}
